52604fa99d99712ab5293277066a9ad7b3254eb4
SiriusNEO <1713833595@qq.com>
2022-10-05 00:06:29 +0800

    update result

---
+++ mlp_show.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+import numpy as np
+import tvm
+from tvm.ir.module import IRModule
+from tvm import topi, relax, te
+from tvm.script import tir as T
+from tvm.script import relax as R
+from tvm.relax.testing import dump_ast
+from utils import LowerToTensorIRPass
+
+@tvm.script.ir_module
+class MultiLayerPerceptron:
+    @R.function
+    def main(x: Tensor((1, 784), "float32"),
+             w0: Tensor((784, 128), "float32"),
+             b0: Tensor((128,), "float32"),
+             w1: Tensor((128, 10), "float32"),
+             b1: Tensor((10,), "float32"),
+             label: Tensor((1,10), "float32")):
+        
+        # block 0
+        with R.dataflow():
+            # linear0
+            lv0 = relax.matmul(x, w0)
+            lv1 = relax.add(lv0, b0)
+            # relu0
+            lv2 = relax.nn.relu(lv1)
+            # linear1
+            lv3 = relax.matmul(lv2, w1)
+            out = relax.add(lv3, b1)
+            loss = relax.nn.softmax_cross_entropy(out, label)
+            R.output(out, loss)
+            
+        return out, loss
+
+print("Before: ")
+MultiLayerPerceptron.show()
+
+
+AutoDiffMLP = relax.transform.SimpleAD(func_name="main", target_names="loss", require_grad_names=["w0", "b0", "w1", "b1"])(MultiLayerPerceptron)
+print("After: ")
+AutoDiffMLP.show()
