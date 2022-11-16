be3d2ba9039bdb4159afaf6f47a7eb15a33dfe7c
ubospica <ubospica@gmail.com>
2022-11-16 21:48:12 +0800

    update

---
+++ conv_numpy_padding_test.py
@@ -0,0 +1,68 @@
+import itertools
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+def grid(*args):
+	return itertools.product(*[range(x) for x in args])
+# nchw x oikk
+# padding = 1
+# output shape: N, O, H, W
+# forward
+def conv2d(input, weight, output):
+	N, C, H, W = input.shape
+	O, I, K, _ = weight.shape
+	P = 1
+	pad_tmp = np.zeros((N, C, H+P*2, W+P*2))
+	for n, c, h, w in grid(N, C, H, W):
+		pad_tmp[n, c, h, w] = input[n, c, h - P, w - P] if h >= P and h <= H + P and w >= P and w <= W + P else 0
+	for n, o, h1, w1, i, k1, k2 in grid(N, O, H - K + 1 + P * 2, W - K + 1 + P * 2, I, K, K):
+		if i == 0 and k1 == 0 and k2 == 0:
+			output[n, o, h1, w1] = 0
+		output[n, o, h1, w1] += pad_tmp[n, i, h1 + k1, w1 + k2] * weight[o, i, k1, k2]
+
+
+def conv2d_adj(input, weight, output, output_adj, input_adj, weight_adj):
+	N, C, H, W = input.shape
+	O, I, K, _ = weight.shape
+	P = 1
+	pad_tmp = np.zeros((N, C, H+P*2, W+P*2))
+	for n, c, h, w in grid(N, C, H + P * 2, W + P * 2):
+		pad_tmp[n, c, h, w] = input[n, c, h - P, w - P] if h >= P and h < H + P and w >= P and w < W + P else 0
+	for n, o, h1, w1, i, k1, k2 in grid(N, O, H - K + 1 + P * 2, W - K + 1 + P * 2, I, K, K):
+		if i == 0 and k1 == 0 and k2 == 0:
+			output[n, o, h1, w1] = 0
+		output[n, o, h1, w1] += pad_tmp[n, i, h1 + k1, w1 + k2] * weight[o, i, k1, k2]
+
+
+	pad_tmp_adj = np.zeros(pad_tmp.shape)
+	# init pad_tmp_adj and weight_adj
+	for n, o, h1, w1, i, k1, k2 in grid(N, O, H - K + 1 + P * 2, W - K + 1 + P * 2, I, K, K):
+		# initing output doesn't influence result
+		weight_adj[o, i, k1, k2] += output_adj[n, o, h1, w1] * pad_tmp[n, i, h1 + k1, w1 + k2]
+		pad_tmp_adj[n, i, h1 + k1, w1 + k2] += output_adj[n, o, h1, w1] * weight[o, i, k1, k2]
+	# init input_adj
+	for n, c, h, w in grid(N, C, H + P * 2, W + P * 2):
+		if h >= P and h < H + P and w >= P and w < W + P:
+			input_adj[n, c, h - P, w - P] += pad_tmp_adj[n, c, h, w]
+
+
+
+input = np.random.randint(0, 3, size=(1, 2, 6, 6)).astype(np.float32)
+weight = np.random.randint(0, 3, size=(1, 2, 3, 3)).astype(np.float32)
+output = np.zeros((1, 1, 6, 6))
+output_adj = np.ones(output.shape)
+input_adj = np.zeros(input.shape)
+weight_adj = np.zeros(weight.shape)
+conv2d_adj(input, weight, output, output_adj, input_adj, weight_adj)
+
+input_t = torch.tensor(input, requires_grad = True)
+weight_t = torch.tensor(weight, requires_grad = True)
+output_t = F.conv2d(input_t, weight_t, padding=1)
+output_t.retain_grad()
+loss_t = torch.sum(output_t)
+loss_t.backward()
+np.testing.assert_equal(output_t.detach().numpy(), output)
+np.testing.assert_equal(output_t.grad.detach().numpy(), output_adj)
+np.testing.assert_equal(weight_t.grad.detach().numpy(), weight_adj)
+np.testing.assert_equal(input_t.grad.detach().numpy(), input_adj)
---
+++ conv_test.py
@@ -0,0 +1,85 @@
+import pytest
+import tvm
+from tvm import relax
+from tvm.error import DiagnosticError
+from tvm.relax.transform import OperatorLegalizer
+from tvm.script._parser import ir as I, relax as R, tir as T
+import tvm.testing
+
+import itertools
+import numpy as np
+
+@I.ir_module
+class Conv2d:
+	@R.function
+	def main(
+		x: R.Tensor((2, 3, 28, 28), "float32"), w: R.Tensor((4, 3, 3, 3), "float32")
+	) -> R.Tensor(None, "float32", ndim=4):
+		gv = R.conv2d(x, w, padding=1, kernel_size=[3, 3])
+		return gv
+Conv2d.show()
+mod = OperatorLegalizer(Conv2d).transform()
+mod.show()
+
+# padding = 0
+# @tvm.script.ir_module
+# class Module:
+#     @R.function
+#     def main(x: Tensor((2, 3, 28, 28), "float32"), w: Tensor((4, 3, 3, 3), "float32")) -> Tensor(None, "float32", ndim = 4):
+#         # block 0
+#         gv = R.call_tir(conv2d, (x, w), (2, 4, 26, 26), dtype="float32")
+#         return gv
+
+#     @T.prim_func
+#     def conv2d(rxplaceholder: T.Buffer[(2, 3, 28, 28), "float32"], rxplaceholder_1: T.Buffer[(4, 3, 3, 3), "float32"], conv2d_nchw: T.Buffer[(2, 4, 26, 26), "float32"]) -> None:
+#         # function attr dict
+#         T.func_attr({"global_symbol": "conv2d", "tir.noalias": True})
+#         # body
+#         # with T.block("root")
+#         pad_temp = T.alloc_buffer([2, 3, 28, 28], dtype="float32")
+#         for i0, i1, i2, i3 in T.grid(2, 3, 28, 28):
+#             with T.block("pad_temp"):
+#                 i0_1, i1_1, i2_1, i3_1 = T.axis.remap("SSSS", [i0, i1, i2, i3])
+#                 T.reads(rxplaceholder[i0_1, i1_1, i2_1, i3_1])
+#                 T.writes(pad_temp[i0_1, i1_1, i2_1, i3_1])
+#                 pad_temp[i0_1, i1_1, i2_1, i3_1] = rxplaceholder[i0_1, i1_1, i2_1, i3_1]
+#         for i0, i1, i2, i3, i4, i5, i6 in T.grid(2, 4, 26, 26, 3, 3, 3):
+#             with T.block("conv2d_nchw"):
+#                 nn, ff, yy, xx, rc, ry, rx = T.axis.remap("SSSSRRR", [i0, i1, i2, i3, i4, i5, i6])
+#                 T.reads(pad_temp[nn, rc, yy + ry, xx + rx], rxplaceholder_1[ff, rc, ry, rx])
+#                 T.writes(conv2d_nchw[nn, ff, yy, xx])
+#                 with T.init():
+#                     conv2d_nchw[nn, ff, yy, xx] = T.float32(0)
+#                 conv2d_nchw[nn, ff, yy, xx] = conv2d_nchw[nn, ff, yy, xx] + pad_temp[nn, rc, yy + ry, xx + rx] * rxplaceholder_1[ff, rc, ry, rx]
+
+
+# padding = 1
+# @tvm.script.ir_module
+# class Module:
+#     @R.function
+#     def main(x: Tensor((2, 3, 28, 28), "float32"), w: Tensor((4, 3, 3, 3), "float32")) -> Tensor(None, "float32", ndim = 4):
+#         # block 0
+#         gv = R.call_tir(conv2d, (x, w), (2, 4, 28, 28), dtype="float32")
+#         return gv
+
+#     @T.prim_func
+#     def conv2d(rxplaceholder: T.Buffer[(2, 3, 28, 28), "float32"], rxplaceholder_1: T.Buffer[(4, 3, 3, 3), "float32"], conv2d_nchw: T.Buffer[(2, 4, 28, 28), "float32"]) -> None:
+#         # function attr dict
+#         T.func_attr({"global_symbol": "conv2d", "tir.noalias": True})
+#         # body
+#         # with T.block("root")
+#         pad_temp = T.alloc_buffer([2, 3, 30, 30], dtype="float32")
+#         for i0, i1, i2, i3 in T.grid(2, 3, 30, 30):
+#             with T.block("pad_temp"):
+#                 i0_1, i1_1, i2_1, i3_1 = T.axis.remap("SSSS", [i0, i1, i2, i3])
+#                 T.reads(rxplaceholder[i0_1, i1_1, i2_1 - 1, i3_1 - 1])
+#                 T.writes(pad_temp[i0_1, i1_1, i2_1, i3_1])
+#                 pad_temp[i0_1, i1_1, i2_1, i3_1] = T.if_then_else(1 <= i2_1 and i2_1 < 29 and 1 <= i3_1 and i3_1 < 29, rxplaceholder[i0_1, i1_1, i2_1 - 1, i3_1 - 1], T.float32(0), dtype="float32")
+#         for i0, i1, i2, i3, i4, i5, i6 in T.grid(2, 4, 28, 28, 3, 3, 3):
+#             with T.block("conv2d_nchw"):
+#                 nn, ff, yy, xx, rc, ry, rx = T.axis.remap("SSSSRRR", [i0, i1, i2, i3, i4, i5, i6])
+#                 T.reads(pad_temp[nn, rc, yy + ry, xx + rx], rxplaceholder_1[ff, rc, ry, rx])
+#                 T.writes(conv2d_nchw[nn, ff, yy, xx])
+#                 with T.init():
+#                     conv2d_nchw[nn, ff, yy, xx] = T.float32(0)
+#                 conv2d_nchw[nn, ff, yy, xx] = conv2d_nchw[nn, ff, yy, xx] + pad_temp[nn, rc, yy + ry, xx + rx] * rxplaceholder_1[ff, rc, ry, rx]
